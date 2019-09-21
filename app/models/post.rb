6d65c17f5d45638164a1d2147fcac192e99ba0d8
Smiley <smiley.smiley1991@gmail.com>
2019-09-21 20:33:39 +0530

    Add missing user association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+
+  # Associations
   has_many :likes, dependent: :destroy
   has_many :comments, as: :commentable
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_082404) do
+ActiveRecord::Schema.define(version: 2019_09_21_145250) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2019_09_21_082404) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -50,4 +52,5 @@ ActiveRecord::Schema.define(version: 2019_09_21_082404) do
 
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
